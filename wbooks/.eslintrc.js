43af79d4af40105930271c92fc5fa8e2045822fa
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2020-10-20 20:28:11 -0300

    Added linter, prettier and fixed base project configurations

--- wbooks/.eslintrc.js
+++ wbooks/.eslintrc.js
@@ -1,6 +1,30 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['wolox-react-native'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    complexity: 'off',
+    'no-nested-ternary': 'off',
+    'no-magic-numbers': 'off',
+    'new-cap': 'off',
+    'comma-dangle': 'off',
+    'import/order': ['error', { 'newlines-between': 'always' }]
+  },
+  settings: {
+    'import/ignore': ['node_modules'],
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        settings: {
+          'import/resolver': {
+            node: {
+              paths: ['src'],
+              extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json']
+            }
+          }
+        }
+      }
+    }
+  }
 };
--- wbooks/babel.config.js
+++ wbooks/babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'import-glob',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json']
+      }
+    ]
+  ]
 };
